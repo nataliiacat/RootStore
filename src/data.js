ea3468e7eacb523b3a62cb6ff1f0aab9a13e88a3
nataliiacat <nataliagricaj6@gmail.com>
2025-01-27 14:58:24 +0100

    add plantsData to data.js

---
+++ src/data.js
@@ -0,0 +1,106 @@
+import carassula from './assets/images/products/kokedama_carassula_hobbit.jpg'
+import coffea from './assets/images/products/kokedama_coffea_arabica.jpg'
+import areca from './assets/images/products/kokedama_areca_lutescens.jpg'
+import epipremnum from './assets/images/products/kokedama_epipremnum_marble_queen.jpg'
+import abidjan from './assets/images/products/ficus_abidjan.jpg'
+import robusta from './assets/images/products/ficus_elastica_robusta.jpg'
+import lyrata from './assets/images/products/ficus_lyrata.jpg'
+import bambino from './assets/images/products/ficus_lyrata_bambino.jpg'
+
+
+const plantsData = [
+  {
+    id: 1,
+    category: "Kokedama",
+    name: "Kokedama Crassula Hobbit",
+    price: 45.99,
+    oldPrice: 60.00,
+    onSale: true,
+    description: "Kokedama is a special way of growing plants, literally translated 'moss ball', originating from Japan. Kokedama consists of a plant, a special substrate ( black clay ketotsuchi ), other components ( Akadama, zeolite, pumice, sand and peat ) are additionally mixed into the clay. From these materials, a suitable plant is perfectly wrapped in a 'ball' and the whole is covered with moss. Traditionally, kokedams are hung on string, macrame or placed on wet pebbles or other material.",
+    care: "We take care of each kokedama according to the type of plant we have in it. Kokedamu water with boiled/boiled water approximately once a week. Kokedama is bothered by chlorine, we do not recommend water directly from the tap. Just place the kokedama in water for a few minutes, let it drain and hang it up again. You can tell if it needs to be watered by the weight of the kokedama and also by how dry your moss ball is. You can moss ball dew to keep the moss bale still moist. Kokedama is not recommended to be placed in direct sunlight.",
+    info: " is best known for its beautifully colored lanceolate leaves that grow in bunches. This particular type of 'snow white' aglaonema attracts attention mainly due to its really white (to slightly yellow) colored leaves, which have green edges and petioles.In the summer, the plant may have delicate inflorescences of yellow or white quivers, which turn into poisonous berries after flowering.Aglaonema belongs to the popular, fast- growing and very undemanding indoor plants, however, due to its white leaves, this cultivar is light - demanding.",
+    image: carassula
+  },
+  {
+    id: 2,
+    category: "Kokedama",
+    name: "Kokedama Coffea arabica",
+    price: 35.00,
+    onSale: false,
+    description: "Kokedama is a special way of growing plants, literally translated 'moss ball', originating from Japan. Kokedama consists of a plant, a special substrate ( black clay ketotsuchi ), other components ( Akadama, zeolite, pumice, sand and peat ) are additionally mixed into the clay. From these materials, a suitable plant is perfectly wrapped in a 'ball' and the whole is covered with moss. Traditionally, kokedams are hung on string, macrame or placed on wet pebbles or other material.",
+    care: "We take care of each kokedama according to the type of plant we have in it. Kokedamu water with boiled/boiled water approximately once a week. Kokedama is bothered by chlorine, we do not recommend water directly from the tap. Just place the kokedama in water for a few minutes, let it drain and hang it up again. You can tell if it needs to be watered by the weight of the kokedama and also by how dry your moss ball is. You can moss ball dew to keep the moss bale still moist. Kokedama is not recommended to be placed in direct sunlight.",
+    info: "Czech coffee plant,  is a useful plant originating from Arabia, which is cultivated for its fruits - coffee beans. The coffee tree has shiny green leaves that are wavy at the edges. Under suitable conditions, tiny white flowers can appear after a few years",
+    image: coffea
+  },
+  {
+    id: 3,
+    category: "Kokedama",
+    name: "Kokedama Areca lutescens",
+    price: 59.99,
+    oldPrice: 83.00,
+    onSale: true,
+    description: "Kokedama is a special way of growing plants, literally translated 'moss ball', originating from Japan. Kokedama consists of a plant, a special substrate ( black clay ketotsuchi ), other components ( Akadama, zeolite, pumice, sand and peat ) are additionally mixed into the clay. From these materials, a suitable plant is perfectly wrapped in a 'ball' and the whole is covered with moss. Traditionally, kokedams are hung on string, macrame or placed on wet pebbles or other material.",
+    care: "We take care of each kokedama according to the type of plant we have in it. Kokedamu water with boiled/boiled water approximately once a week. Kokedama is bothered by chlorine, we do not recommend water directly from the tap. Just place the kokedama in water for a few minutes, let it drain and hang it up again. You can tell if it needs to be watered by the weight of the kokedama and also by how dry your moss ball is. You can moss ball dew to keep the moss bale still moist. Kokedama is not recommended to be placed in direct sunlight.",
+    info: "'dypsis lutescens' from the areca family, ( Czech areca palm or areca palm) is a plant native to Madagascar. It belongs to the heat-loving species and can grow to around 1.5 m in our conditions. A small plant usually has two leaves, over time the branches branch slightly into more leaves. Unlike other palm trees, the soft, deep green leaves do not sting, so they are safer. Due to the appearance of its leaves, it is also called the butterfly palm. The undemanding palm tree is suitable for almost any environment and beautifully fills an empty space both at home and in the office or in corridors.",
+    image: areca
+  },
+  {
+    id: 4,
+    category: "Kokedama",
+    name: "Kokedama Epipremnum Marble Queen",
+    price: 45.95,
+    onSale: false,
+    description: "Kokedama is a special way of growing plants, literally translated 'moss ball', originating from Japan. Kokedama consists of a plant, a special substrate ( black clay ketotsuchi ), other components ( Akadama, zeolite, pumice, sand and peat ) are additionally mixed into the clay. From these materials, a suitable plant is perfectly wrapped in a 'ball' and the whole is covered with moss. Traditionally, kokedams are hung on string, macrame or placed on wet pebbles or other material.",
+    care: "We take care of each kokedama according to the type of plant we have in it. Kokedamu water with boiled/boiled water approximately once a week. Kokedama is bothered by chlorine, we do not recommend water directly from the tap. Just place the kokedama in water for a few minutes, let it drain and hang it up again. You can tell if it needs to be watered by the weight of the kokedama and also by how dry your moss ball is. You can moss ball dew to keep the moss bale still moist. Kokedama is not recommended to be placed in direct sunlight.",
+    info: " is a variegated variant with green leaves that resemble marble in their annealing - the degree of variegation is mainly determined by light conditions. This plant is very popular for its unpretentiousness, easy and fast propagation and is suitable for beginners.",
+    image: epipremnum
+  },
+  {
+    id: 5,
+    category: "Ficuses",
+    name: "Ficus Abidjan",
+    price: 45.95,
+    onSale: false,
+    description: "Ficus elastica Robusta or also known as the rubber tree, which was previously used to produce rubber, comes from India. It is an evergreen tree-like plant, which is typical for its large and shiny leathery leaves. Ficuses are among the undemanding, fast-growing houseplants that contain latex - a white liquid. Ficuses can be shaped and thickened by pruning.",
+    care: "We take care of each kokedama according to the type of plant we have in it. Kokedamu water with boiled/boiled water approximately once a week. Kokedama is bothered by chlorine, we do not recommend water directly from the tap. Just place the kokedama in water for a few minutes, let it drain and hang it up again.",
+    info: " is particularly attractive due to its dark green to black leaves. Ficuses can be shaped and thickened by pruning.",
+    image: abidjan
+  },
+  {
+    id: 6,
+    category: "Ficuses",
+    name: "Ficus Robusta",
+    price: 45.95,
+    oldPrice: 70.00,
+    onSale: true,
+    description: "Ficus elastica Robusta or also known as the rubber tree, which was previously used to produce rubber, comes from India. It is an evergreen tree-like plant, which is typical for its large and shiny leathery leaves. Ficuses are among the undemanding, fast-growing houseplants that contain latex - a white liquid. Ficuses can be shaped and thickened by pruning.",
+    care: "We take care of each kokedama according to the type of plant we have in it. Kokedamu water with boiled/boiled water approximately once a week. Kokedama is bothered by chlorine, we do not recommend water directly from the tap. Just place the kokedama in water for a few minutes, let it drain and hang it up again. ",
+    info: " is particularly attractive due to its dark green to black leaves. Ficuses can be shaped and thickened by pruning.",
+    image: robusta
+  },
+  {
+    id: 7,
+    category: "Ficuses",
+    name: "Ficus Lyrata",
+    price: 35.95,
+    oldPrice:50.00,
+    onSale: false,
+    description: "Ficus Lyrata is a tree-like plant characterized by its large green leaves of a violin (lyre) shape with a wavy edge. In room conditions, these leaves with pronounced veining can grow up to 30 cm in size. It comes from tropical Africa. Ficuses are among the unpretentious, fast-growing indoor plants that contain latex - a white liquid. If the plant is not limited by a low ceiling, it can grow to a height of several meters. Ficuses can be shaped and thickened by pruning.",
+    care: "We take care of each kokedama according to the type of plant we have in it. Kokedamu water with boiled/boiled water approximately once a week. Kokedama is bothered by chlorine, we do not recommend water directly from the tap. Just place the kokedama in water for a few minutes, let it drain and hang it up again. You can tell if it needs to be watered by the weight of the kokedama and also by how dry your moss ball is.",
+    info: " stands out in large spaces with its exotic appearance. It needs support to grow. The lower leaves may drop over time, which is normal for older specimens of this variety.",
+    image: lyrata
+  },
+  {
+    id: 8,
+    category: "Ficuses",
+    name: "Ficus Lyrata Bambino",
+    price: 22.50,
+    onSale: false,
+    description: "Ficus Lyrata is a tree-like plant characterized by its large green violin-shaped (lyre-shaped) leaves with a wavy edge and pronounced veining. Ficus Lyrata bambino (in English Dwarf fiddle leaf fig ) is a dwarf cultivar that, unlike the classic Lyrata, saves space and grows smaller (approx. 1 m). It comes from tropical Africa. Ficuses are among the unpretentious, fast-growing indoor plants that contain latex - a white liquid. Ficuses can be shaped and thickened by pruning.",
+    care: "We take care of each kokedama according to the type of plant we have in it. Kokedamu water with boiled/boiled water approximately once a week. Kokedama is bothered by chlorine, we do not recommend water directly from the tap. Just place the kokedama in water for a few minutes, let it drain and hang it up again. You can tell if it needs to be watered by the weight of the kokedama and also by how dry your moss ball is.",
+    info: " stands out in large spaces with its exotic appearance. It needs support to grow. The lower leaves may drop over time, which is normal for older specimens of this variety.",
+    image: bambino
+  }
+];
+
+export default plantsData;
