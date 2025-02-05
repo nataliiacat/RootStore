a462d4734a25a6566883af926da88437fa4cc084
nataliiacat <nataliagricaj6@gmail.com>
2025-02-05 23:59:36 +0100

    add section journey

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './AboutSection.css'
+import packing from '../../assets/images/packing.jpg'
+import delivery from '../../assets/images/delivery.jpg'
+import unboxing from '../../assets/images/unboxing.jpg'
 
 const AboutSection = () => {
   return (
@@ -14,6 +17,35 @@ const AboutSection = () => {
               <p className="text">But we don’t stop at plants. Our experts have handpicked the finest pots, tools, and plant care essentials to help you nurture your new green friends. We’re constantly updating our range, so you’ll always find the latest and best products to support your plant journey. Root Store is here to make sure you have everything you need to grow into a true plant enthusiast.</p>
               <button className="btn discover-btn"> LEARN MORE</button>
             </div>
+          </div>
+        </div>
+      </section>
+      <section className="journey container">
+        <div className="journey-item flex">
+          <div className="journey-content">
+            <h3 className="subtitle">01. Carefully Packed</h3>
+            <p className="text">Each plant is hand-selected and securely packed in eco-friendly packaging to ensure it arrives in perfect condition.</p>
+          </div>
+          <div className="journey-img packing-img">
+            <img src={packing} alt="image packing" />
+          </div>
+        </div>
+        <div className="journey-item flex">
+          <div className="journey-img">
+            <img src={delivery} alt="image delivery" />
+          </div>
+          <div className="journey-content">
+            <h3 className="subtitle">02. Fast & Safe Shipping</h3>
+            <p className="text">We ship your plant as soon as possible, using reliable carriers to ensure a smooth and timely delivery.</p>
+          </div>
+        </div>
+        <div className="journey-item flex">
+          <div className="journey-content">
+            <h3 className="subtitle">03. Unboxing & Thriving</h3>
+            <p className="text">Unpack your new green friend, follow our care guide, and enjoy the beauty it brings to your space!</p>
+          </div>
+          <div className="journey-img">
+            <img src={unboxing} alt="image unboxing" />
           </div>
         </div>
       </section>
