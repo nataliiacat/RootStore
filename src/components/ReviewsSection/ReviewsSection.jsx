bbaae9b2f6667ce827250cde3cd37aa1fd2ba853
nataliiacat <nataliagricaj6@gmail.com>
2025-03-06 22:09:28 +0100

    added styles for section reviews

--- src/components/ReviewsSection/ReviewsSection.jsx
+++ src/components/ReviewsSection/ReviewsSection.jsx
@@ -1,12 +1,12 @@
 import React, { useRef } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, EffectCreative } from 'swiper/modules'
+import { Navigation } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/navigation'
-import 'swiper/css/effect-creative'
 import './ReviewsSection.css'
 import { reviewsData } from '../../data'
 import StarRating from '../StarRating/StarRating'
+import { IoArrowBackCircleSharp, IoArrowForwardCircleSharp } from 'react-icons/io5'
 
 const ReviewsSection = () => {
   const prevRef = useRef(null);
@@ -16,50 +16,41 @@ const ReviewsSection = () => {
     <>
       <section className="reviewsSection">
         <div className="container">
-          <div className="textDiv">
+          <div className="title-contain">
             <h1 className="title">What People Say</h1>
-            <p className="text">Our clients love our service, and we love them back!</p>
+            <p className="text">Our clients love our service, and we truly value them in return. We’re dedicated to building strong, lasting relationships.</p>
             <div className="nav-buttons">
-              <button ref={prevRef} className="custom-prev">←</button>
-              <button ref={nextRef} className="custom-next">→</button>
+              <button ref={prevRef} className="custom-prev">
+                <IoArrowBackCircleSharp className="icon" />
+              </button>
+              <button ref={nextRef} className="custom-next">
+                <IoArrowForwardCircleSharp className="icon" />
+              </button>
             </div>
           </div>
           <div className="slider-block">
             <Swiper
-              modules={[Navigation, EffectCreative]}
+              modules={[Navigation]}
               slidesPerView={1}
-              navigation={{ prevEl: prevRef.current, nextEl: nextRef.current }}
-              onInit={(swiper) => {
-                swiper.params.navigation.prevEl = prevRef.current;
-                swiper.params.navigation.nextEl = nextRef.current;
-                swiper.navigation.init();
-                swiper.navigation.update();
+              navigation={{
+                prevEl: prevRef.current,
+                nextEl: nextRef.current
               }}
-              effect="creative"
-              creativeEffect={{
-                prev: {
-                  shadow: true,
-                  translate: [0, 0, -400],
-                },
-                next: {
-                  translate: ["100%", 0, 0],
-                },
-              }}
-              touchEventsTarget="container"
-              simulateTouch={true}
+              loop={true}
               grabCursor={true}
             >
               {reviewsData.map((review) => (
-                <SwiperSlide key={review.id} className="rewiew">
-                  <img
+                <SwiperSlide key={review.id} className="review">
+                  <div className="flex"><img
                     src={review.image}
                     alt={review.name}
                     className="review-img"
                   />
-                  <h3 className="subtitle">{review.name}</h3>
+                    <h3 className="subtitle">{review.name}</h3>
+                  </div><p className="text">{review.review}</p>
+                  <StarRating className="ratings" rating={review.rating} />
                   <p className="info">{review.plant}</p>
-                  <StarRating rating={review.rating} />
-                  <p className="text">{review.review}</p>
+
                 </SwiperSlide>
               ))}
             </Swiper>
